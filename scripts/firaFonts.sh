2993db7fca6c71eb6d6021612da49e4e0b325366
HaoZeke <driger633@gmail.com>
2017-11-15 07:35:13 +0530

    ci: Better handling of caches

--- scripts/firaFonts.sh
+++ scripts/firaFonts.sh
@@ -1,7 +1,12 @@
 cd $HOME/save
 
-aria2c --continue=true "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
-aria2c --continue=true "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"
+if [ ! -f FiraFonts4106.zip ]; then
+    aria2c "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
+fi
+
+if [ ! -f FiraMonoFonts3206.zip ]; then
+    aria2c "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"
+fi
 
 unzip -n FiraFonts4106.zip
 unzip -n FiraMonoFonts3206.zip
